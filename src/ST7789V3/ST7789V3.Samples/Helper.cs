70cb7eaa967847807b494c8fbf91b2226097bc69
Gil Zhang <gil.zhang.dev@outlook.com>
2022-11-13 19:18:19 +0800

    添加像素转换代码

---
+++ src/ST7789V3/ST7789V3.Samples/Helper.cs
@@ -0,0 +1,59 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace LedMatrix.Helpers
+{
+    static class Helper
+    {
+        private static string FILE_LOCATION = "/dev/fb1";
+        public static void WriteToFile(byte[] contentToWrite)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(FILE_LOCATION, FileMode.Open)))
+            {
+                for (int i = 0; i < contentToWrite.Length; ++i)
+                {
+                    writer.Seek(i, SeekOrigin.Begin);
+                    writer.Write(contentToWrite[i]);
+                }
+            }
+        }
+
+        public static byte[] ConvertToByteArray(this short[] source)
+        {
+            byte[] arrayAsByte = new byte[source.Length * 2/*sizeof short*/];
+            Buffer.BlockCopy(source, 0, arrayAsByte, 0, arrayAsByte.Length);
+            return arrayAsByte;
+        }
+
+        public static byte[] ImageToRgb565(this Image<Bgr565> original, int width, int height)
+        {
+            Int16[] pixelList = new Int16[width * height];
+
+            int idx = 0; //each iteration increments this value
+            for (int columnIdx = 0; columnIdx < original.Height; ++columnIdx)
+            {
+                for (int rowIdx = 0; rowIdx < original.Width; ++rowIdx)
+                {
+                    var imagePx = original[rowIdx, columnIdx].PackedValue;
+
+                    Console.WriteLine($"x:{original[rowIdx, columnIdx].ToVector3().X}y:{original[rowIdx, columnIdx].ToVector3().Y}z:{original[rowIdx, columnIdx].ToVector3().Z}");
+
+                    pixelList[idx++] = (Int16)imagePx;
+                }
+            }
+            return pixelList.ConvertToByteArray();
+        }
+
+        public static Int16 CreatePixelFromRgb(byte r, byte g, byte b)
+        {
+            return (Int16)((r << 11) | (g << 5) | b);
+        }
+
+        public static void Rotate(this Image<Bgr565> original, int angle)
+        {
+            if (angle == 0) return;
+            original.Mutate(x => x.Rotate(angle));
+        }
+    }
+}
--- src/ST7789V3/ST7789V3.Samples/Program.cs
+++ src/ST7789V3/ST7789V3.Samples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Iot.Device.ST7789V3;
+using LedMatrix.Helpers;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using System.Device.Pwm.Drivers;
@@ -8,6 +9,7 @@ var resetPin = 27;
 var dataCommandPin = 25;
 var backlightPin = 18;
 var blFreq = 1000;
+
 // SPI0 CS0
 SpiConnectionSettings senderSettings = new(0, 0)
 {
@@ -21,11 +23,8 @@ var pwmChannel = new SoftwarePwmChannel(pinNumber: backlightPin, frequency: blFr
 
 var lcd = new ST7789V3(dataCommandPin, senderDevice, resetPin, pwmChannel, shouldDispose: false);
 
-
 lcd.Reset();
-
 lcd.Init();
-
 lcd.SetWindows(0, 0, 172, 320);
 
 var imageFilePath = "./Pic/LCD_1inch47.jpg";
@@ -33,9 +32,11 @@ var imageFilePath = "./Pic/LCD_1inch47.jpg";
 
 var image = Image.Load<Bgr565>(imageFilePath);
 
-var dataList = new byte[172 * 320 * 2];
+//var dataList = new byte[172 * 320 * 2];
 
-image.CopyPixelDataTo(dataList);
+//image.CopyPixelDataTo(dataList);
+
+var dataList = image.ImageToRgb565(image.Width, image.Height);
 
 lcd.SpiWrite(true, new ReadOnlySpan<byte>(dataList));
 

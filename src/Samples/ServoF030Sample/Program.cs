49c909a7528070911b13d7bfc50a194ac1d793bf
gil zhang <gil.zhang.dev@outlook.com>
2024-12-04 01:15:56 +0800

    update f030 i2c demo code

--- src/Samples/ServoF030Sample/Program.cs
+++ src/Samples/ServoF030Sample/Program.cs
@@ -5,16 +5,45 @@ try
     while (true)
     {
         using I2cDevice i2c = I2cDevice.Create(new I2cConnectionSettings(1, 0x06));
+
         byte[] writeBuffer = new byte[5] { 0xff, 0x01, 0x00, 0x00, 0x00 };
         byte[] receiveData = new byte[5];
+
         i2c.WriteRead(writeBuffer, receiveData);
+
+        for (int i = -90; i < 90; i += 1)
+        {
+            float angle = i;
+
+            byte[] angleBytes = BitConverter.GetBytes(angle);
+
+            writeBuffer[0] = 0x01;
+            Array.Copy(angleBytes, 0, writeBuffer, 1, angleBytes.Length);
+
+            i2c.WriteRead(writeBuffer, receiveData);
+            Thread.Sleep(20);
+        }
+        for (int i = 90; i > -90; i -= 1)
+        {
+            float angle = i;
+
+            byte[] angleBytes = BitConverter.GetBytes(angle);
+
+            writeBuffer[0] = 0x01;
+            Array.Copy(angleBytes, 0, writeBuffer, 1, angleBytes.Length);
+
+            i2c.WriteRead(writeBuffer, receiveData);
+            Thread.Sleep(20);
+        }
+
+
         Console.WriteLine($"I2C 设备连接成功--{DateTime.Now.ToString("s")}");
         foreach (var data in receiveData)
         {
             Console.Write($"{data}, ");
         }
-        Console.WriteLine();
-        Thread.Sleep(500);
+        //Console.WriteLine();
+        //Thread.Sleep(500);
     }
 }
 catch (Exception ex)
